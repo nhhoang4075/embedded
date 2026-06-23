3a88908291e518af3edcc2e0921c4fc4f98647b7
Nguyễn Huy Hoàng <nhh4075@gmail.com>
2026-06-23 11:00:55 +0700

    Bypass STMPE811 touch init (I2C timeout hang) + auto-skip Screen1
    STMPE811 does not respond on I2C on this board, so BSP_TS_Init hangs
    ~12s per call (HAL_I2C timeout). The game only uses the analog
    joystick, so touch is not needed:
    - STM32TouchController::init() skips BSP_TS_Init
    - sampleTouch() always returns false
    - Screen1Presenter::activate() auto-goto Screen2 (can't tap Start)

--- projects/game2048/TouchGFX/gui/src/screen1_screen/Screen1Presenter.cpp
+++ projects/game2048/TouchGFX/gui/src/screen1_screen/Screen1Presenter.cpp
@@ -1,5 +1,6 @@
 #include <gui/screen1_screen/Screen1View.hpp>
 #include <gui/screen1_screen/Screen1Presenter.hpp>
+#include <gui/common/FrontendApplication.hpp>
 
 Screen1Presenter::Screen1Presenter(Screen1View& v)
     : view(v)
@@ -9,7 +10,10 @@ Screen1Presenter::Screen1Presenter(Screen1View& v)
 
 void Screen1Presenter::activate()
 {
-
+    /* Touch bị disable (STMPE811 không phản hồi) -> không bấm được nút Start.
+     * Auto chuyển sang Screen2 (màn game). */
+    static_cast<FrontendApplication*>(Application::getInstance())
+        ->gotoScreen2ScreenSlideTransitionEast();
 }
 
 void Screen1Presenter::deactivate()
